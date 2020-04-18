69070d25c32b2105c6105bfaa57247c29ab0f00a
allen <allen.simbul591@gmail.com>
2020-04-18 15:53:04 +0100

    added sorting to tasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -21,9 +21,16 @@ router.post('/tasks', auth, async (req, res) => {
 // Read ALL Tasks associated with a user that is logged in and authenticated
 router.get('/tasks', auth, async (req, res) => {
   const match = {};
+  const sort = {};
 
   if (req.query.completed) {
     match.completed = req.query.completed === 'true';
+  }
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(':');
+
+    sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
   }
 
   try {
@@ -34,6 +41,7 @@ router.get('/tasks', auth, async (req, res) => {
         options: {
           limit: parseInt(req.query.limit),
           skip: parseInt(req.query.skip),
+          sort,
         },
       })
       .execPopulate();
