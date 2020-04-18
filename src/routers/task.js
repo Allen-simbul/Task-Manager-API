b41066955e4532da68648b24bfb11982c6b73c69
allen <allen.simbul591@gmail.com>
2020-04-18 14:23:32 +0100

    Filter get ALL tasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -20,8 +20,19 @@ router.post('/tasks', auth, async (req, res) => {
 
 // Read ALL Tasks associated with a user that is logged in and authenticated
 router.get('/tasks', auth, async (req, res) => {
+  const match = {};
+
+  if (req.query.completed) {
+    match.completed = req.query.completed === 'true';
+  }
+
   try {
-    await req.user.populate('tasks').execPopulate();
+    await req.user
+      .populate({
+        path: 'tasks',
+        match,
+      })
+      .execPopulate();
     res.send(req.user.tasks);
   } catch (e) {
     res.status(500).send(e);
